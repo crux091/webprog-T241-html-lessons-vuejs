093e1968d74ed9c9a34b3b31302317c7efcd2886
Rick Francis Cruz <kikorickcruz@gmail.com>
2026-01-27 09:12:09 +0800

    fix

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import website from './components/ResumeWebsite'
 
 const app = createApp(App)
 app.component('comment-form', CommentForm)
